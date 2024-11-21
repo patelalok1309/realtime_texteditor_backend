98c3387beec230cd4884c1be902b13a041b0bd5d
AALOK PATEL <aalok.patel@primedemo.in>
2024-11-21 13:02:00 +0530

    fixed route import issues

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import TextEditor from "./screens/TextEditor/TextEditor.jsx";
+import TextEditor from "../screens/TextEditor/TextEditor.jsx";
 
-import App from "./App";
+import App from "../App.js";
 
 const router = createBrowserRouter([
     {
